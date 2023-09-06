9a4f6f6015c6b63e248355eaae3ead6f700b7c11
amb231143 <amb231143@uvg.edu.gt>
2023-09-06 14:50:17 -0600

    Update Sede.java

--- Ejercicio3/Sede.java
+++ Ejercicio3/Sede.java
@@ -1,17 +1,25 @@
 
+/**
+ * Esta clase es donde se crea la lista de los estudiantes y la sede
+ * @author: Dulce Ambrosio
+ * @version: 05/09/2023
+ */
 import java.util.ArrayList;
 
 public class Sede {
+    // Atributos
     private String LugarSede;
     private String NuevaSede;
     private ArrayList<Estudiante> estudiante;
 
+    // controlador
     public Sede(String lugarSede, String nuevaSede, ArrayList<Estudiante> estudiante) {
         this.LugarSede = lugarSede;
         this.NuevaSede = nuevaSede;
         this.estudiante = new ArrayList<>();
     }
 
+    // getters y setters
     public String getLugarSede() {
         return LugarSede;
     }
@@ -36,10 +44,16 @@ public class Sede {
         this.estudiante = estudiante;
     }
 
+    /**
+     * Método para añadir los atributos del estudiante a la lista
+     */
     public void registrarEstudiante(Estudiante estudiantes) {
         estudiante.add(estudiantes);
     }
 
+    /**
+     * Método para mostrar los datos del estudiante
+     */
     public void mostrarEstudiantes() {
         System.out.println("Estudiantes en la sede " + getNuevaSede() + ":");
         for (Estudiante estudiante : estudiante) {
