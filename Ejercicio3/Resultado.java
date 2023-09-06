1b59fb7a04069a345c08e5b724ccf8dec32fb1d2
amb231143 <amb231143@uvg.edu.gt>
2023-09-06 14:49:36 -0600

    Update Resultado.java

--- Ejercicio3/Resultado.java
+++ Ejercicio3/Resultado.java
@@ -1,12 +1,21 @@
+/**
+ * Esta clase es donde se crean los atributos de los resultados
+ * 
+ * @author: Dulce Ambrosio
+ * @version: 05/09/2023
+ */
 public class Resultado {
+    // Atributos
     private String materia;
     private double nota;
 
+    // controlador
     public Resultado(String materia, double nota) {
         this.materia = materia;
         this.nota = nota;
     }
 
+    // getters y setters
     public String getMateria() {
         return materia;
     }
